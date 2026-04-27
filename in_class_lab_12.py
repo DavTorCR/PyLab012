47ba1eda7af183ad1d2258d895a20abd890d9ad1
DavTorCR <hollow.prolifc.vegabond@gmail.com>
2026-04-27 15:17:52 -0700

    This is the start of the Lab 12

---
+++ in_class_lab_12.py
@@ -0,0 +1,84 @@
+print("hello world. . . ")
+
+# "does anyone remember composition?
+# answer given; it concerns the order of creating class objects.
+# "Sort of, it concerns the composition of class objects "
+# the underscore operator
+# it overrides the ==
+
+# what is  __init__ ?
+# what is  self ?
+
+# in python, self is still used, but not strictly necessary
+
+# how is self used?
+# instance-iation
+# when you copy mutable objects, only the pointers get copied.
+
+# in the object orientated programming world,
+# directly accessing and mutating objects in unadvised.
+
+# what makes it a method instead of a function?
+# because it is inside of the claas object,
+# and its the first instance, the instance itself. (self)
+
+# ^ this is the proper method to access and mutate an object
+
+# create a method to modify it, is the preffered means of
+# accessing and mutating an object.
+
+# how do create a private varibale ?
+# adding an undecore to the definition
+
+# why would you make a private varibale ?
+# in c sharp and ++, there are setters and
+# getters for private variables
+# what are 'getters and 'setters' ?
+# if another programmer only has a getter in their code,
+# it means they do not want their variables to be changed
+# (muatted)
+
+# object composition although simple, it is a
+# key stone concept that will be brought up
+# over and over again
+
+# how do we mutable our objects?
+# we create a method with variables
+# and input variables suing method
+
+# recursion and memo-ization will continue coming up
+# over and over again.
+
+print("This is the start of the Lab 12.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
